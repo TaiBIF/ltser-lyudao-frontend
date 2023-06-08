76f568b92ffb1697f2af5de56749b415bbb8815c
Allie <huaruhsiao.b@gmail.com>
2023-06-08 16:14:21 +0800

    [UPDATE] replace related literature list with api data

---
+++ src/components/RelatedLiterature/SearchIcon.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const SearchIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="21.53"
+        height="21.53"
+        viewBox="0 0 21.53 21.53"
+      >
+        <g
+          id="Group_7726"
+          data-name="Group 7726"
+          transform="translate(-1619 -631)"
+        >
+          <g
+            id="Ellipse_2947"
+            data-name="Ellipse 2947"
+            transform="translate(1619 631)"
+            fill="none"
+            stroke="#529a81"
+            strokeLinecap="round"
+            strokeWidth={2}
+          >
+            <circle cx="8.512" cy="8.512" r="8.512" stroke="none" />
+            <circle cx="8.512" cy="8.512" r="7.512" fill="none" />
+          </g>
+          <line
+            id="Line_312"
+            data-name="Line 312"
+            x2="5.478"
+            y2="5.475"
+            transform="translate(1633.638 645.641)"
+            fill="none"
+            stroke="#529a81"
+            strokeLinecap="round"
+            strokeWidth={2}
+          />
+        </g>
+      </svg>
+    </>
+  );
+};
+
+export default SearchIcon;
--- src/pages/RelatedLiterature.tsx
+++ src/pages/RelatedLiterature.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Banner from 'components/Banner';
 import Breadcrumb from 'components/Breadcrumb';
 import Pagination from 'components/Pagination';
+import SearchIcon from 'components/RelatedLiterature/SearchIcon';
 
 import literatureBannerImg from 'image/literature_bn.png';
 
@@ -10,11 +11,17 @@ import { BannerData } from 'types/common';
 import { literatureList } from 'data/literature';
 import { LiteratureItem } from 'types/literature';
 
+import useRender from 'hooks/useRender';
+import { LITERATURE_API_URL } from 'data/api';
+
 const RelatedLiterature = () => {
   const [filter, setFilter] = useState({
     keyword: '',
   });
+  const [literatureList, setLiteratureList] = useState<LiteratureItem[]>([]);
   const [data, setData] = useState<LiteratureItem[]>([]);
+  const { getList } = useRender();
+
   const bannerData: BannerData = {
     title: '相關文獻',
     en: ['Related', 'literature'],
@@ -22,15 +29,27 @@ const RelatedLiterature = () => {
     bgImg: literatureBannerImg,
   };
 
+  const isFetchingList = setLiteratureList.length === 0;
+
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilter({ ...filter, keyword: e.currentTarget.value });
   };
 
   useEffect(() => {
-    const matchKeyword = literatureList.filter((v) =>
-      v.name.includes(filter.keyword)
-    );
-    setData([...matchKeyword]);
+    getList({
+      url: LITERATURE_API_URL,
+      setList: setLiteratureList,
+      defaultList: literatureList,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isFetchingList && filter.keyword) {
+      const matchKeyword = literatureList.filter((v) =>
+        v.name.includes(filter.keyword)
+      );
+      setData([...matchKeyword]);
+    }
   }, [filter.keyword]);
 
   return (
@@ -49,51 +68,18 @@ const RelatedLiterature = () => {
                   onChange={handleSearchChange}
                 />
                 <button>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="21.53"
-                    height="21.53"
-                    viewBox="0 0 21.53 21.53"
-                  >
-                    <g
-                      id="Group_7726"
-                      data-name="Group 7726"
-                      transform="translate(-1619 -631)"
-                    >
-                      <g
-                        id="Ellipse_2947"
-                        data-name="Ellipse 2947"
-                        transform="translate(1619 631)"
-                        fill="none"
-                        stroke="#529a81"
-                        strokeLinecap="round"
-                        strokeWidth={2}
-                      >
-                        <circle cx="8.512" cy="8.512" r="8.512" stroke="none" />
-                        <circle cx="8.512" cy="8.512" r="7.512" fill="none" />
-                      </g>
-                      <line
-                        id="Line_312"
-                        data-name="Line 312"
-                        x2="5.478"
-                        y2="5.475"
-                        transform="translate(1633.638 645.641)"
-                        fill="none"
-                        stroke="#529a81"
-                        strokeLinecap="round"
-                        strokeWidth={2}
-                      />
-                    </g>
-                  </svg>
+                  <SearchIcon />
                 </button>
               </div>
             </div>
-            <ul className="literature-list">
-              {data.map((v) => {
-                const { id, name } = v;
-                return <li key={id}>{name}</li>;
-              })}
-            </ul>
+            {!isFetchingList && (
+              <ul className="literature-list">
+                {data.map((v) => {
+                  const { id, name } = v;
+                  return <li key={id}>{name}</li>;
+                })}
+              </ul>
+            )}
             <Pagination />
           </div>
         </div>
