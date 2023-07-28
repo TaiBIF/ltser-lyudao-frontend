c7be43b0c2d51deed77b3fc05c96230302e3349a
Allie <huaruhsiao.b@gmail.com>
2023-07-28 23:53:25 +0800

    [FIX] config url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 // export const API_URL = `https://.../api`;
-export const API_URL = `www.thousandaitech.com/api`;
-export const BE_URL = `www.thousandaitech.com`;
-export const IMAGE_URL = `www.thousandaitech.com`;
+export const API_URL = `https://www.thousandaitech.com/api`;
+export const BE_URL = `https://www.thousandaitech.com`;
+export const IMAGE_URL = `https://www.thousandaitech.com`;
 
 export const DEPOSITAR_API_URL = `https://data.depositar.io/api/3/action/datastore_search`;
