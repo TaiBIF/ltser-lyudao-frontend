f205da6ab5c0b2d86885f8f2b88c031e1fccce5a
Allie <huaruhsiao.b@gmail.com>
2023-08-08 22:26:07 +0800

    [FIX] staff protected route bug

--- src/utils/ProtectedRoute/Staff.tsx
+++ src/utils/ProtectedRoute/Staff.tsx
@@ -2,12 +2,15 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { useAuthContext } from 'context/AuthContext';
 import { swalToast } from 'helpers/customSwal';
 
-interface Staff {
+interface StaffProps {
   redirectPath?: string;
   children?: any;
 }
 
-const Staff: React.FC<Staff> = ({ redirectPath = '/', children }) => {
+const Staff: React.FC<StaffProps> = ({
+  redirectPath = '/dashboard/about',
+  children,
+}) => {
   const { group } = useAuthContext();
   const isStaff = group === 'staff';
 
