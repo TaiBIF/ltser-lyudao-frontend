34f3a547278bd4d5061972a512f3b3440ba7176f
Allie <huaruhsiao.b@gmail.com>
2024-04-11 19:49:31 +0800

    [UPDATE] social-economy-data content

--- src/components/SiteData/SocialEconomyData/Page/Industry.tsx
+++ src/components/SiteData/SocialEconomyData/Page/Industry.tsx
@@ -56,7 +56,7 @@ const Industry = () => {
       </Grid>
       <section className="u-section">
         <Typography variant="body1" component={'div'} textAlign={'end'}>
-          資料來源：SEGIS 社會經濟資料服務平台
+          資料來源：交通部觀光署觀光統計資料庫、SEGIS 社會經濟資料服務平台
         </Typography>
       </section>
     </>
