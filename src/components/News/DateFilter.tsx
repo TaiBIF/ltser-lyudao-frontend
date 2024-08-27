94166a6c1803632df8e23a0d2f46cf3c75574a8c
JJJHANG <jangzhu5678@gmail.com>
2024-08-27 13:32:24 +0800

    fix: change placeholder of data type input based on website language in news page

--- src/components/News/DateFilter.tsx
+++ src/components/News/DateFilter.tsx
@@ -6,6 +6,8 @@ import { NewsFilterItem, NewsItem } from 'types/news';
 import useRender from 'hooks/page/useRender';
 import { NEWS_API_URL } from 'data/api';
 import { PaginationDataItem } from 'types/utils';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 interface DateFilterProps {
   filter: NewsFilterItem;
@@ -28,13 +30,28 @@ const DateFilter = (props: DateFilterProps) => {
     I18N_KEY_PREFIX,
   } = props;
 
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const placeholderText = i18n.language === 'zh-tw' ? '年-月-日' : 'yyyy-mm-dd';
 
   const hasStartDate = filter.startDate !== '';
   const hasEndDate = filter.endDate !== '';
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilter({ ...filter, [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  const handleStartDateChange = (date: Date | null) => {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      startDate: date ? date.toISOString().split('T')[0] : '',
+    }));
+  };
+
+  const handleEndDateChange = (date: Date | null) => {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      endDate: date ? date.toISOString().split('T')[0] : '',
+    }));
   };
 
   const { getList } = useRender();
@@ -59,32 +76,42 @@ const DateFilter = (props: DateFilterProps) => {
       <form className="tool-select c-form" onSubmit={handleDateSubmit}>
         <div className="date-box">
           <div className="inp-item c-form__set">
-            <input
-              type="date"
-              name="startDate"
+            <DatePicker
+              selected={filter.startDate ? new Date(filter.startDate) : null}
+              onChange={handleStartDateChange}
+              placeholderText={placeholderText}
+              dateFormat="yyyy-MM-dd"
+              showYearDropdown
+              dateFormatCalendar="MMMM"
+              yearDropdownItemNumber={30}
+              scrollableYearDropdown
               className="c-form__date"
-              onChange={handleInputChange}
-              value={filter.startDate}
             />
             <div className="c-form__icon">
               <DateIcon />
             </div>
           </div>
-          <span>~</span>
+          <span>～</span>
           <div className="inp-item c-form__set">
-            <input
-              type="date"
-              name="endDate"
+            <DatePicker
+              selected={filter.endDate ? new Date(filter.endDate) : null}
+              onChange={handleEndDateChange}
+              placeholderText={placeholderText}
+              dateFormat="yyyy-MM-dd"
+              showYearDropdown
+              dateFormatCalendar="MMMM"
+              yearDropdownItemNumber={30}
+              scrollableYearDropdown
               className="c-form__date"
-              onChange={handleInputChange}
-              value={filter.endDate}
             />
             <div className="c-form__icon">
               <DateIcon />
             </div>
           </div>
         </div>
-        <button type="submit">{t(`${I18N_KEY_PREFIX}.filterBtn`)}</button>
+        <button type="submit" className="submit-btn">
+          {t(`${I18N_KEY_PREFIX}.filterBtn`)}
+        </button>
       </form>
     </>
   );
