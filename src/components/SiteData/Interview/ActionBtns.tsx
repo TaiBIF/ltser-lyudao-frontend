738e603bb8fa7747121337f21f81c2f2ec094940
JJJHANG <jangzhu5678@gmail.com>
2025-12-24 10:51:00 +0800

    update: remove prev & next btn

--- src/components/SiteData/Interview/ActionBtns.tsx
+++ src/components/SiteData/Interview/ActionBtns.tsx
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-import NextIcon from 'components/NextIcon';
-import PrevIcon from 'components/PrevIcon';
+// import NextIcon from 'components/NextIcon';
+// import PrevIcon from 'components/PrevIcon';
 
-import { interviewList } from 'data/siteData';
+// import { interviewList } from 'data/siteData';
 import { useNavigate } from 'react-router-dom';
 
 const ActionBtns = () => {
   const { interviewId } = useParams();
-  const id = Number(interviewId);
+  // const id = Number(interviewId);
 
-  const hasPrev = interviewId && id - 1 >= 1;
-  const hasNext = interviewId && id + 1 <= interviewList.length;
+  // const hasPrev = interviewId && id - 1 >= 1;
+  // const hasNext = interviewId && id + 1 <= interviewList.length;
 
   const navigate = useNavigate();
 
   return (
     <>
-      {hasPrev && (
+      {/* {hasPrev && (
         <Link
           to={`/site-data/social-observation/social-interview-data/${id - 1}`}
         >
           <PrevIcon />
           <p>上一則</p>
         </Link>
-      )}
+      )} */}
       <a
         onClick={() =>
           navigate('/site-data/social-observation/social-interview-data')
@@ -33,14 +33,14 @@ const ActionBtns = () => {
       >
         返回列表
       </a>
-      {hasNext && (
+      {/* {hasNext && (
         <Link
           to={`/site-data/social-observation/social-interview-data/${id + 1}`}
         >
           <p>下一則</p>
           <NextIcon />
         </Link>
-      )}
+      )} */}
     </>
   );
 };
